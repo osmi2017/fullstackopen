3729ecc6807bf7598b07787597a82cc872c1d9b6
Soroismael <soro.ismael@financialxs.com>
2025-06-30 22:18:04 +0000

    part1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
+  const Header = (props) =>{
+	  return(
+	  <h1>{props.course}</h1>
+	  )
+  }
+  
+  const Content =() =>{
+	  return (
+    <div>
+      <Part  part={part1} exercises={exercises1 }/>
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3}/>
+    </div>
+  )
+  }
+  
+  const Total =(props) =>{
+	  return(
+	  <p>Number of exercises {props.sumexercice}</p>
+	  )
+  }
+  
+  const Part =(props) =>{
+	  return(
+	  <p>
+        {props.part} {props.exercises}
+      </p>
+	  )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total sumexercice={exercises1 + exercises2 + exercises3}/>
+    
+    </div>
+  )
+}
+
+export default App
